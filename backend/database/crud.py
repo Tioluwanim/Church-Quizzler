2a7ee77bd410f6a22651cd344f52dc03bd2e2df2
Tioluwanim <tolutemitiwa@gmail.com>
2025-10-01 03:10:41 +0100

    trying to deploy

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from .database.models import Team, Question, Score
+from .models import Team, Question, Score
 from sqlalchemy import func
 
 # =====================
